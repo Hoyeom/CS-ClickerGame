ccd5a1dad2bcbd3c1201787076b54f7c3eadd31e
Hoyeom <korehdrb@gmail.com>
2022-06-12 16:13:27 +0900

    Excel To Csv

---
+++ ClickerGame/Assets/Editor/Scripts/DataTransformer.cs
@@ -0,0 +1,84 @@
+using System;
+using System.IO;
+using System.Reflection;
+using Data;
+using UnityEditor;
+using UnityEngine;
+
+public class DataTransformer : EditorWindow
+{
+    private static readonly string TablePath = $"{Application.dataPath}/Editor/Data/Csv/";
+    private static readonly string SavePath = $"{Application.dataPath}/Resources/Data/";
+    private static readonly string AssetPath = $"Assets/Resources/Data/";
+    private static readonly string[] TableNames = Enum.GetNames(typeof(Define.Table));
+
+    private static readonly string Format = ".csv";
+
+    [MenuItem("Parser/LoadAllData")]
+    private static void LoadAllData()
+    {
+        CreatePath();
+        LoadData<StatusData>(Define.Table.Status,1);
+        LoadData<MonsterData>(Define.Table.Monster, 1);
+        LoadData<UpgradeData>(Define.Table.Upgrade, 1);
+        LoadData<WeaponData>(Define.Table.Weapon, 1);
+        LoadData<ShopData>(Define.Table.Shop, 1);
+        LoadData<StringData>(Define.Table.String, 1);
+        LoadData<PathData>(Define.Table.Path, 1);
+    }
+
+    private static void LoadData<T>(Define.Table table = Define.Table.None, int fieldLine = 2)
+        where T : ScriptableObject, ITableSetter
+    {
+        string tableName = TableNames[(int) table];
+
+        if(Directory.Exists($"{SavePath}{tableName}"))
+            Directory.Delete($"{SavePath}{tableName}", true);
+        Directory.CreateDirectory($"{SavePath}{tableName}");
+
+        StreamReader reader = new StreamReader($"{TablePath}{tableName}{Format}");
+
+        for (int i = 0; i < fieldLine - 1; i++)
+            reader.ReadLine();
+
+        string[] fieldNames = reader.ReadLine().Split(',');
+        
+        while (!reader.EndOfStream)
+        {
+            string[] data = reader.ReadLine().Split(',');
+            T sO = ScriptableObject.CreateInstance<T>();
+            SetData(sO, data, fieldNames);
+            AssetDatabase.CreateAsset(sO, $"{AssetPath}{tableName}/{sO.GetID().ToString()}.asset");
+        }
+
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+    }
+
+    private static void CreatePath()
+    {
+        if (!Directory.Exists(TablePath))
+            Directory.CreateDirectory(TablePath);
+
+        if (!Directory.Exists(SavePath))
+            Directory.CreateDirectory(SavePath);
+    }
+
+    private static void SetData(UnityEngine.Object obj, string[] data, string[] fieldNames)
+    {
+        for (int i = 0; i < fieldNames.Length; i++)
+        {
+            FieldInfo info = obj.GetType().GetField(fieldNames[i],
+                BindingFlags.Public |
+                BindingFlags.Instance);
+
+            if (info == null)
+                throw new Exception($"Field Name: {fieldNames[i]} / Data: {data[i]}");
+
+            info.SetValue(obj,
+                info.FieldType.IsEnum
+                    ? Enum.Parse(info.FieldType, data[i])
+                    : Convert.ChangeType(data[i], info.FieldType));
+        }
+    }
+}
---
+++ ExcelToCsv/ExcelToCsv/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ExcelToCsv
+{
+    internal class Program
+    {
+        private static string tablePath = null;
+        private static string csvPath = null;
+        
+        public static void Main(string[] args)
+        {
+            InitPath();
+
+            if(!Directory.Exists(tablePath))
+                Console.WriteLine($"Missing Directory: {tablePath}");
+
+            CreatePath(csvPath);
+
+            OpenExcel(out Excel.Workbook xlWorkBook);
+            
+            foreach (Excel.Worksheet sheet in xlWorkBook.Worksheets)
+                SaveSheet(sheet, csvPath);
+            
+            xlWorkBook.Close(false);
+        }
+
+        
+        private static void InitPath()
+        {
+            Directory.SetCurrentDirectory($"{Directory.GetCurrentDirectory()}/Assets");
+            
+            tablePath = $"{Directory.GetCurrentDirectory()}/Editor/Data/Excel/";
+            csvPath = $"{Directory.GetCurrentDirectory()}/Editor/Data/Csv/";
+        }
+
+        private static void OpenExcel(out Excel.Workbook xlWorkBook)
+        {
+            Excel.Application xlApp = new Excel.ApplicationClass();
+            
+            xlWorkBook = xlApp.Workbooks.Open($"{tablePath}ExcelData.xlsx");
+
+            xlApp.Visible = true;
+            xlApp.DisplayAlerts = false;
+        }
+
+        private static void SaveSheet(Excel.Worksheet sheet,string savePath)
+        {
+            if(sheet.Name.Contains("_")) return;
+            
+            string sheetPath = $"{savePath}{sheet.Name}.csv";
+
+            sheet.Select();
+
+            sheet.SaveAs(sheetPath, 62);
+        }
+
+        private static void CreatePath(string path)
+        {
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+        }
+    }
+}
