205072c6dc7f6dffbadaf940b5d27d78febf223f
Hoyeom <korehdrb@gmail.com>
2022-06-27 22:10:19 +0900

    Addressable Assets Setting

--- ClickerGame/Assets/Scripts/Manager/ResourceManager.cs
+++ ClickerGame/Assets/Scripts/Manager/ResourceManager.cs
@@ -10,12 +10,21 @@ namespace Manager
 {
     public class ResourceManager
     {
+        private AsyncOperationHandle _loadHandle;
+
+        public void AsyncDataLoad<T>(Action<List<T>> completeCallback) where T : ScriptableObject, ITableSetter
+        {
+            GetDataPath<T>(out string path);
+            AsyncOperationHandle<IList<T>> f = Addressables.LoadAssetsAsync<T>(path, (result) => { });
+            
+            _loadHandle = Addressables.DownloadDependenciesAsync(path);
+            _loadHandle.Completed += Addressables.Release;
+        }
         public Object Load(string path)
         {
             PathNullException(path);
             return Resources.Load(path);
         }
-
         public T Load<T>(string path) where T : Object
         {
             PathNullException(path);
@@ -28,7 +37,6 @@ namespace Manager
 
             return Resources.Load<T>(path);
         }
-
         public AsyncOperationHandle<IList<T>> AsyncLoadData<T>(Dictionary<int,T> dataDic) where T : ScriptableObject, ITableSetter
         {
             string dataName = typeof(T).Name;
@@ -42,27 +50,16 @@ namespace Manager
 
             return Addressables.LoadAssetsAsync<T>($"Data/{dataName}", sO => dataDic.Add(sO.GetID(), sO));
         }
-        
         public T[] LoadData<T>() where T : ScriptableObject,ITableSetter
         {
-            string dataName = typeof(T).Name;
-
-            int index = dataName.IndexOf("Data");
-
-            if (index > 0)
-                dataName = dataName.Remove(index, 4);
-            
-            // Debug.Log($"Complete Load {dataName}");
-
-            return Resources.LoadAll<T>($"Data/{dataName}");
+            GetDataPath<T>(out string path);
+            return Resources.LoadAll<T>(path);
         }
-
         private void PathNullException(string path)
         {
             if (String.IsNullOrEmpty(path))
                 throw new Exception($"Load Failed: Empty Path");
         }
-
         public GameObject Instantiate(string path)
         {
             if(!path.Contains("Prefabs/"))
@@ -75,19 +72,26 @@ namespace Manager
             
             return Instantiate(obj);
         }
-        
         public GameObject Instantiate(GameObject obj)
         {
             return Object.Instantiate(obj);
         }
-
         public void Destroy(Object obj)
         {
             if(obj == null)
                 return;
-
             Object.Destroy(obj);
         }
-        
+        private void GetDataPath<T>(out string path) where T : ScriptableObject, ITableSetter
+        {
+            path = typeof(T).Name;
+
+            int index = path.IndexOf("Data");
+
+            if (index > 0)
+                path = path.Remove(index, 4);
+
+            path = $"Data/{path}";
+        }
     }
 }
